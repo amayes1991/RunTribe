eff18c6dceca3f7504e24ef20ecc7167da54df87
Austin Mayes <austinmayes@gmail.com>
2025-12-17 20:47:11 -0600

    add Analytics

--- runtribe/app/layout.js
+++ runtribe/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
           {children}
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
